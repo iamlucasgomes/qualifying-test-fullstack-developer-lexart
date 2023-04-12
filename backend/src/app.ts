7afef88873bc3520b9f3cbac6432112218dc5724
iamlucasgomes <lucas.devjs@gmail.com>
2023-04-12 11:59:50 -0300

    Chore: Backend setado

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,31 @@
-import express from 'express';
+import express = require('express');
+class App {
+  public app: express.Express;
 
-const app = express();
+  constructor() {
+    this.app = express();
 
-app.use(express.json());
+    this.config();
 
-export default app;
+    // Não remover essa rota
+    this.app.get('/', (req, res) => res.json({ ok: true }));
+  }
+
+  private config(): void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
