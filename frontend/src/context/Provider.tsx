69f02c616fdb82c9f00276cd560e4e76b3789013
iamlucasgomes <lucas.devjs@gmail.com>
2023-04-12 09:37:26 -0300

    Chore: Arquivos movi os arquivos da aplicação para a pasta frontend, para separar a estrutura do backend e frontend

--- frontend/src/context/Provider.tsx
+++ frontend/src/context/Provider.tsx
@@ -1,12 +1,12 @@
 import { ReactNode, useState, useMemo } from "react";
-import  { MyContextData, AppContext }  from ".";
+import { MyContextData, AppContext } from ".";
 
 interface IProps {
   children: ReactNode
 }
 
 export const AppContextProvider = ({ children }: IProps) => {
-  
+
   const [searchTerm, setSearchTerm] = useState('')
   const [selectedPlatform, setSelectedPlatform] = useState('todas')
   const [selectedCategory, setSelectedCategory] = useState('Categorias')
@@ -21,6 +21,6 @@ export const AppContextProvider = ({ children }: IProps) => {
   };
 
   return <AppContext.Provider value={context}>
-    {children}
-    </AppContext.Provider>
+          {children}
+         </AppContext.Provider>
 }
