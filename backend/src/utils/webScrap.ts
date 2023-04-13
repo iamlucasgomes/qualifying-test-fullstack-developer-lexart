f27f434b2ee13c6ceadb46f17f49534a83515878
iamlucasgomes <lucas.devjs@gmail.com>
2023-04-13 11:37:55 -0300

    feat: webscrap mercado livre função parseMercadoLivre concluída

--- backend/src/utils/webScrap.ts
+++ backend/src/utils/webScrap.ts
@@ -7,6 +7,7 @@ interface Product {
   description: string;
   image: string;
   category: string;
+  link: string;
 }
 
 function parseMercadoLivre(html: string, category: string): Product[] {
@@ -16,16 +17,18 @@ function parseMercadoLivre(html: string, category: string): Product[] {
   $('#root-app > div > div.ui-search-main.ui-search-main--only-products.ui-search-main--with-topkeywords.shops__search-main > section > ol >').each((_, element) => {
     const titleElem = $(element).find('h2.ui-search-item__title');
     const priceTextElem = $(element).find('span.price-tag-fraction').first();
-    const descriptionElem = $(element).find('div.ui-search-item__group__element.ui-search-item__description > p');
+    const descriptionElem = $(element).find('span.ui-search-item__variations-text');
     const imageElem = $(element).find('img.ui-search-result-image__element');
+    const linkElem = $(element).find('a.ui-search-link');
 
     try {
       const title = titleElem.text();
       const price = parseFloat(priceTextElem.text().replace(/\.|,/g, match => match === '.' ? '' : '.'));
       const description = descriptionElem.text() || '';
-      const image = imageElem.attr('src') || '';
-      console.log(price);
-      products.push({ title, price, description, image, category });
+      const image = imageElem.attr('data-src') || '';
+      const link = linkElem.attr('href') || '';
+      
+      products.push({ title, price, description, image, category, link });
     } catch (error) {
       console.error('Error parsing product:', error);
     }
@@ -46,7 +49,10 @@ function parseBuscape(html: string, category: string): Product[] {
       const description = $(element).find('div.product-description').text() || '';
       const image = $(element).find('img.product-image').attr('src') || '';
 
-      products.push({ title, price, description, image, category });
+      products.push({
+        title, price, description, image, category,
+        link: ''
+      });
     } catch (error) {
       console.error('Error parsing product:', error);
     }
