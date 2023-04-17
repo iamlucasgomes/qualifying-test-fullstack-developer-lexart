9aed99ced3c5a9ccafbd92409bd6a436f98033e8
iamlucasgomes <lucas.devjs@gmail.com>
2023-04-16 21:55:35 -0300

    Feat: controller refatorei a controller pra ficar mais performatico

--- backend/src/controller/WebScrap.controller.ts
+++ backend/src/controller/WebScrap.controller.ts
@@ -1,41 +1,47 @@
-import { Request, Response, NextFunction, response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import searchProducts from '../utils/webScrap'
 import RequestScrapService from '../service/RequestScrap.service';
 import IProduct from '../interface/IProduct';
 import RequestScrap from '../database/models/RequestScraps';
 
 export default class WebScrapController {
-  private req: Request;
-  private res: Response;
-  private next: NextFunction;
   private Service: RequestScrapService;
 
-  constructor(req: Request, res: Response, next: NextFunction) {
-    this.req = req;
-    this.res = res;
-    this.next = next;
+  constructor(private req: Request, private res: Response, private next: NextFunction) {
     this.Service = new RequestScrapService();
   }
 
+  private sendResponse(data: any) {
+    const response = Array.isArray(data) && data.length > 0 ? data.at(0) : [];
+    this.res.status(200).json(response);
+  }
+
   public getWebScrap = async () => {
-    const { searchTerm, category, platform } = this.req.body;
-    const exist = await this.Service.getAllScraps({ searchTerm, category, platform })
-
-    if (exist.length === 0) {
-      const scrap: IProduct[] = await searchProducts(searchTerm, category, platform);
-
-      const response: RequestScrap[] = await this.Service.insertWebScrap(this.req.body, scrap)
-      Array.isArray(response) && response.length > 0
-        ? this.res.status(200).json(response[0]) : this.res.status(200).json([])
+    try {
+      const { searchTerm, category, platform } = this.req.body;
+      const existingScrap = await this.Service.getAllScraps({ searchTerm, category, platform });
+    
+      if (existingScrap.length === 0) {
+        const scrap: IProduct[] = await searchProducts(searchTerm, category, platform);
+        const newScrap: RequestScrap[] = await this.Service.insertWebScrap(this.req.body, scrap);
+        this.sendResponse(newScrap);
+      } else {
+        this.sendResponse(existingScrap);
+      }
     }
-    Array.isArray(exist) && exist.length > 0
-    && this.res.status(200).json(exist[0])
+    catch (error) {
+      this.next(error);
+    }
   };
 
   public getAllWebScrap = async () => {
-    const { searchTerm, category } = this.req.body;
-    const response: RequestScrap[] = await this.Service.getAllScraps({ searchTerm, category })
-    Array.isArray(response) && response.length > 0
-      ? this.res.status(200).json(response[0]) : this.res.status(200).json([])
-  }
+    try {
+      const { searchTerm, category } = this.req.body;
+      const scraps: RequestScrap[] = await this.Service.getAllScraps({ searchTerm, category });
+      this.sendResponse(scraps);
+    }
+    catch (error) {
+      this.next(error);
+    }
+  };
 }
